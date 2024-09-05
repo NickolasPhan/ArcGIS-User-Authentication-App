e29225efb18079eb360ba413bc5d2217e9edc099
NickolasPhan <67134735+NickolasPhan@users.noreply.github.com>
2024-09-05 14:23:00 -0400

    changes + gitignore

--- token_server.js
+++ token_server.js
@@ -7,37 +7,45 @@ const port = 8000;
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.post('/', (req, res) => {
+async function get_accessToken(refreshToken) {
+    const url = 'https://www.arcgis.com/sharing/rest/oauth2/token?client_id=raIlJpkSMoStBPOs&grant_type=refresh_token&refresh_token=' + refreshToken
+
+    return new Promise((resolve) => {
+        https.get(url, res => {
+            let data = [];
+    
+            res.on('data', chunk => {
+                data.push(chunk);
+            }); // end https_res.on data
+    
+            res.on('end', () => {
+                var content = JSON.parse(Buffer.concat(data).toString());
+                // console.log('\n' + content.access_token + '\n');
+    
+                fs.writeFile('./access_token.txt', JSON.stringify(content, null, 4), err => {
+                    if (err) {
+                        console.error(err)
+                    } else {
+                        console.log("Access token file written successfully");
+                    } // end if else
+                }) // end writeFile
+    
+                resolve(content.access_token);
+            }) // end https_res.on end
+        }).on('error', err => {
+            console.log("Error: " + err.message);
+        }); // end https.get
+    }); // end return Promise
+} // end get_accessToken
+
+app.post('/', async (req, res) => {
     res.setHeader('content-type', 'application/json');
     res.setHeader('Access-Control-Allow-Origin', '*');
 
-    refreshToken = req.body.refreshToken;
-    let access_token_link = "https://www.arcgis.com/sharing/rest/oauth2/token?client_id=raIlJpkSMoStBPOs&grant_type=refresh_token&refresh_token=" + refreshToken;
+    let access_token = await get_accessToken(req.body.refreshToken);
 
-    https.get(access_token_link, https_res => {
-        let data = [];
-
-        https_res.on('data', chunk => {
-            data.push(chunk);
-        }); // end https_res.on data
-
-        https_res.on('end', () => {
-            var content = JSON.parse(Buffer.concat(data).toString());
-            // console.log(content);
-
-            fs.writeFile('./access_token.txt', JSON.stringify(content, null, 4), err => {
-                if (err) {
-                    console.error(err)
-                } else {
-                    console.log("Access token file written successfully");
-                } // end if else
-            }) // end writeFile
-        }) // end https_res.on end
-    }).on('error', err => {
-        console.log("Error: " + err.message);
-    }); // end https.get
-    
-    res.sendStatus(200);
+    // res.sendStatus(200);
+    res.send({'Access Token': access_token});
 }); // end app.get
 
 app.listen(port, () => {
